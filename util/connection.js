8e29ab82678a5457b49ebe039850fc391a849323
AbhayBhan <abhaybhan197@gmail.com>
2024-06-11 13:59:35 +0530

    Fix : Fixed The Mongo Connection String

--- util/connection.js
+++ util/connection.js
@@ -1,7 +1,7 @@
 //mongoose
 const mongoose = require("mongoose");
 
-mongoose.connect(`mongodb+srv://metubeapp1:wDf2ol6IYQBeFy4I@cluster0.briwpaq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`, {
+mongoose.connect(`mongodb+srv://metubeapp1:wDf2ol6IYQBeFy4I@cluster0.briwpaq.mongodb.net/metube?retryWrites=true&w=majority&appName=Cluster0`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   //useFindAndModify: false,
