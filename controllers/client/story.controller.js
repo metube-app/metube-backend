4752a3408af50244124d1caff02c499c3c32e9a9
AbhayBhan <abhaybhan197@gmail.com>
2024-06-18 20:13:21 +0530

    Feat : Added Get all stories code

--- controllers/client/story.controller.js
+++ controllers/client/story.controller.js
@@ -1,3 +1,4 @@
+const followsModel = require("../../models/follows.model");
 const storyModel = require("../../models/story.model");
 const userModel = require("../../models/user.model");
 const { uploadObjectsToS3 } = require("../../util/awsFunction");
@@ -57,6 +58,91 @@ exports.storyUpload = async (req, res) => {
   }
 };
 
+exports.getFollowerStories = async (req, res) => {
+  try {
+    const { userId } = req.body;
+
+    if (!userId) {
+      return res
+        .status(400)
+        .json({ status: false, message: "UserID not found" });
+    }
+
+    const user = await userModel.findById(userId);
+
+    if (!user) {
+      return res.status(400).json({ status: false, message: "User not found" });
+    }
+
+    const storyObj = await storyModel.findOne({ userId: user._id });
+
+    const followObj = await followsModel.findOne({ email: user.email });
+
+    const following = followObj.following;
+
+    const now = new Date();
+    const futureDate = new Date();
+    futureDate.setTime(now.getTime() + 24 * 60 * 60 * 1000);
+
+    const posts = storyObj.stories
+      .filter((story) => new Date(story.expiresAt) <= futureDate)
+      .map((story) => {
+        return {
+          story_id: story._id,
+          id: story._id,
+          user_id: user._id,
+          url: story.link,
+          type: story.type,
+          create_date: story.createdAt,
+          username: user.fullName,
+          profile_pic: user.image,
+          story_image: null,
+        };
+      });
+
+    following.forEach(async (flw) => {
+      const now = new Date();
+      const futureDate = new Date();
+      futureDate.setTime(now.getTime() + 24 * 60 * 60 * 1000);
+
+      const otherUser = await userModel.findOne({ email: flw });
+
+      const otherStoryObj = await storyModel.findOne({ userId: otherUser._id });
+
+      otherStoryObj.stories
+        .filter((story) => new Date(story.expiresAt) <= futureDate)
+        .forEach((story) => {
+          posts.push({
+            story_id: story._id,
+            id: story._id,
+            user_id: otherUser._id,
+            url: story.link,
+            type: story.type,
+            create_date: story.createdAt,
+            username: otherUser.fullName,
+            profile_pic: otherUser.image,
+            story_image: null,
+          });
+        });
+    });
+
+    const response = {
+      status: "success",
+      msg: "Story Successfully Retrieved",
+      user_id: user._id,
+      profile_pic: user.image,
+      post: posts,
+    };
+
+    return res.status(200).json(response);
+  } catch (error) {
+    return res.status(500).json({
+      status: false,
+      message: error.message || "Internal Server Error",
+    });
+  }
+};
+
 exports.getStories = async (req, res) => {
   try {
     const { userId } = req.body;
@@ -91,16 +177,16 @@ exports.getStories = async (req, res) => {
           create_date: story.createdAt,
           username: user.fullName,
           profile_pic: user.image,
-          story_image: null
+          story_image: null,
         };
       });
-    
+
     const response = {
-      status : "success",
-      msg : "Story Successfully Retrieved",
-      user_id : user._id,
-      profile_pic : user.image,
-      post : posts
+      status: "success",
+      msg: "Story Successfully Retrieved",
+      user_id: user._id,
+      profile_pic: user.image,
+      post: posts,
     };
 
     return res.status(200).json(response);
--- routes/client/story.route.js
+++ routes/client/story.route.js
@@ -7,10 +7,12 @@ const storage = require("../../util/multer");
 const upload = multer({ storage });
 
 const checkAccessWithSecretKey = require("../../checkAccess");
-const { storyUpload, getStories } = require("../../controllers/client/story.controller");
+const { storyUpload, getStories, getFollowerStories } = require("../../controllers/client/story.controller");
 
 route.put("/upload-file", upload.single("content"), checkAccessWithSecretKey(), storyUpload);
 
 route.post("/get-stories", checkAccessWithSecretKey(), getStories);
+
+route.post("/get-all-stories", checkAccessWithSecretKey(), getFollowerStories);
 
 module.exports = route;
